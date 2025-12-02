091a42a82fe4842b28ab5665c6136b192ca5ae72
Romain Delage <romaindelage@MacBook-Air-de-Romain-2.local>
2025-12-02 10:05:03 +0100

    Added schema.sql, auto created on app start

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS utilisateur (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  role TEXT,
+  username VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS parking (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  nom VARCHAR(255),
+  ylat FLOAT,
+  xlong FLOAT,
+  coord_x FLOAT,
+  coord_y FLOAT,
+  nb_places INT,
+  nb_pr INT,
+  nb_pmr INT,
+  nb_voitures_electriques INT,
+  nb_velo INT,
+  nb_2r_el INT,
+  nb_autopartage INT,
+  nb_2_rm INT
+);
+
+CREATE TABLE IF NOT EXISTS donnees (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  parking_id UUID,
+  date_comptage TIMESTAMP,
+  nb_places_disponible INT,
+  nb_pr_dispo INT,
+  nb_pmr_dispo INT,
+  nb_voitures_electriques_dispo INT,
+  nb_velo_dispo INT,
+  nb_2r_el_dispo INT,
+  nb_autopartage_dispo INT,
+  nb_2_rm_dispo INT,
+  CONSTRAINT fk_donnees_parking FOREIGN KEY (parking_id) REFERENCES parking(id) ON DELETE CASCADE
+);
