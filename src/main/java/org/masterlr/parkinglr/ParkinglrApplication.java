27e4899bdecc75f0939b9d040a981b99d20be9cc
Romain Delage <romaindelage@MacBook-Air-de-Romain-2.local>
2025-12-02 09:44:36 +0100

    Init

---
+++ src/main/java/org/masterlr/parkinglr/ParkinglrApplication.java
@@ -0,0 +1,13 @@
+package org.masterlr.parkinglr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinglrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/masterlr/parkinglr/ParkinglrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.masterlr.parkinglr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
